740a98b0f6c36bd59708550053abb36cc4e45d32
hannah-bannanah <74356906+Hannah-bannanah@users.noreply.github.com>
2022-10-17 09:55:23 +0200

    renamed client service test class

--- src/test/java/com/dogginer/dog/service/ClientServiceImplTest.java
+++ src/test/java/com/dogginer/dog/service/ClientServiceImplTest.java
@@ -16,7 +16,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @TestPropertySource("/application.properties")
 @SpringBootTest
-public class ClientServiceImplTests {
+public class ClientServiceImplTest {
 
     @Autowired
     BCryptPasswordEncoder passwordEncoder;
